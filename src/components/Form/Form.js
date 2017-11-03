d48766109d60ffefcae2af441805b042e6ba03fa
Leticia Rezende <euamoaita@gmail.com>
2017-11-02 20:39:12 -0400

    Created Function to get each input value
    One function to rule them all.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,54 +1,143 @@
-import React from 'react';
-import './jquery.creditCardValidator.js';
+import React, { Component } from 'react';
+//import './jquery.creditCardValidator.js';
+//import './scripts.js';
+//import PropTypes from 'prop-types';
+import InputMask from 'react-input-mask';
+
+class Form extends Component {
+	constructor(props) {
+		super(props);
+		this.changeValue = this.changeValue.bind(this);
+		this.state = {
+			optionSelected: false,
+			cardNumber: '',
+			cardHolder: '',
+			month: '',
+			year: '',
+			csc: '',
+			monthOptions: [
+			{
+			  name: 'MM',
+			  value: null,
+			  isDisabled: true,
+			  selected: true,
+			},
+			{
+			  name: '01',
+			  value: '01',
+			},
+			{
+			  name: '02',
+			  value: '02',
+			},
+			{
+			  name: '03',
+			  value: '03',
+			},
+			{
+			  name: '04',
+			  value: '04',
+			},
+			{
+			  name: '05',
+			  value: '05',
+			},
+			{
+			  name: '06',
+			  value: '06',
+			},
+			{
+			  name: '07',
+			  value: '07',
+			},
+			{
+			  name: '08',
+			  value: '08',
+			},
+			{
+			  name: '09',
+			  value: '09',
+			},
+			{
+			  name: '10',
+			  value: '10',
+			},
+			{
+			  name: '11',
+			  value: '11',
+			},
+			{
+			  name: '12',
+			  value: '12',
+			},
+			],
+			yearOptions: [
+			{
+			  name: 'YY',
+			  value: null,
+			  isDisabled: true,
+			  selected: true,
+			},
+			{
+			  name: '17',
+			  value: '17',
+			},
+			{
+			  name: '18',
+			  value: '18',
+			},
+			{
+			  name: '19',
+			  value: '19',
+			},
+			],
+		};
+	}
+	
 
 
-const Form = () => {
-  return (
-    <form>
-		<div className="credit-card">
-			<input type="text" name="credit-card-number" id="credit-card-input" placeholder="Credit Card Number" autoFocus />
-			<span></span>
-		</div>
-		<input type="text" name="credit-card-name" id="name-input"  placeholder="Name on Credit Card" />
-		<div className="row">
-			<div className="col-xs-4">
-				<select id="month" name="month">
-					<option value="" disabled selected>MM</option>
-					<option value="01">01</option>
-					<option value="02">02</option>
-					<option value="03">03</option>
-					<option value="04">04</option>
-					<option value="05">05</option>
-					<option value="06">06</option>
-					<option value="07">07</option>
-					<option value="08">08</option>
-					<option value="09">09</option>
-					<option value="10">10</option>
-					<option value="11">11</option>
-					<option value="12">12</option>
-				</select>
-			</div>
-			<div className="col-xs-4">
-				<select id="year" name="year">
-					<option value="" disabled selected>YY</option>
-					<option value="17">17</option>
-					<option value="18">18</option>
-					<option value="19">19</option>
-					<option value="20">20</option>
-					<option value="21">21</option>
-					<option value="22">22</option>
-					<option value="23">23</option>
-					<option value="24">24</option>
-					<option value="25">25</option>
-				</select>
-			</div>
-			<div className="col-xs-4">
-				<input className="security-number" type="number" name="security-number" placeholder="CSC" />
-			</div>
-		</div>
-		<button className="submit" type="submit" disabled><i className="fa fa-check"></i> Pay Now</button>
-	</form>
-  );
+	// optionSelected = event => {
+	// 	this.setState({optionSelected: true});
+	// 	console.log(this.state.optionSelected);
+	// }
+
+
+	changeValue(evt){
+		let inputfield = '';
+	    console.log([evt.target.name] + ': ' + evt.target.value);
+	    this.setState={[evt.target.name]: evt.target.value};
+	    return inputfield;
+	}
+
+	render() {
+		const createOptions = (item, key) =>
+			<option key={key} value={item.value} selected={item.selected}  disabled={item.isDisabled}> {item.name} </option>;
+	  	return (
+		    <form>
+				<div className='credit-card'>
+					<InputMask className='security-number'  mask='9999  9999  9999 9999 999' name='cardNumber' id='credit-card-input' placeholder='Credit Card Number' maskChar=''  onChange={this.changeValue}  autoFocus/>
+					<span></span>
+				</div>
+				<InputMask name='cardHolder' id='name-input'  placeholder='Name on Credit Card' formatChars='a' onChange={this.changeValue} />
+				<div className='row'>
+					<div className='col-xs-4'>
+						<select onChange={this.changeValue} id='month' name='month'>
+							{this.state.monthOptions.map(createOptions)}
+						</select>
+					</div>
+					<div className='col-xs-4'>
+						<select onChange={this.changeValue} id='year' name='year'>
+							{this.state.yearOptions.map(createOptions)}
+						</select>
+					</div>
+					<div className='col-xs-4'>
+						<InputMask className='security-number'  mask='999' placeholder='CSC' onChange={this.changeValue}  maskChar='' name='csc'/>
+					</div>
+				</div>
+				<button className='submit' type='submit' disabled><i className='fa fa-check'></i> Pay Now</button>
+			</form>
+	  	);
+	};
 };
 
 export default Form;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import './assets/css/font-awesome.min.css';
 import App from './App.js';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
